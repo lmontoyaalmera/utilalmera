f71f58da50f382a1feadcd8b646b57c9848547c6
gfigueroa <gfigueroa@almeraim.com>
2019-05-14 14:14:55 -0500

    Arreglo a asyntask

--- utilalmeralib/src/main/java/com/almera/utilalmeralib/lib_util_network/LibNetworkUtil.java
+++ utilalmeralib/src/main/java/com/almera/utilalmeralib/lib_util_network/LibNetworkUtil.java
@@ -1,30 +1,43 @@
 package com.almera.utilalmeralib.lib_util_network;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 
 import java.net.URL;
 import java.net.URLConnection;
 
 public class LibNetworkUtil {
     @SuppressLint("CheckResult")
-    public static void isConnectedToServer(final String url, final int timeout, final LibUtilConnectionLisener connectionLisener) {
-       new Thread(new Runnable() {
-           @Override
-           public void run() {
-               try {
-                   URL myUrl = new URL(url);
-                   URLConnection connection = myUrl.openConnection();
-                   connection.setConnectTimeout(timeout);
-                   connection.connect();
-                   connectionLisener.onRequestConnectionServer(true);
-               } catch (Exception e) {
-                   // Handle your exceptions
-                   connectionLisener.onRequestConnectionServer(false);
-               }
-           }
-       }).start();
+    public static void isConnectedToServer(final String url, final int timeout,  LibUtilConnectionLisener connectionLisener) {
+        TestConnection testConnection=new TestConnection(connectionLisener);
+        testConnection.execute(url,timeout+"");
 
 
+    }
+    public static class TestConnection extends AsyncTask<String, Void,Boolean>{
+        LibUtilConnectionLisener libUtilConnectionLisener;
+        public TestConnection(LibUtilConnectionLisener libUtilConnectionLisener) {
+            this.libUtilConnectionLisener=libUtilConnectionLisener;
+        }
 
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try{
+                URL myUrl = new URL(strings[0]);
+                URLConnection connection = myUrl.openConnection();
+                connection.setConnectTimeout(Integer.parseInt(strings[1]));
+                connection.connect();
+                return true;
+            }catch (Exception e){
+                return false;
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+          libUtilConnectionLisener.onRequestConnectionServer(result);
+        }
     }
 }
