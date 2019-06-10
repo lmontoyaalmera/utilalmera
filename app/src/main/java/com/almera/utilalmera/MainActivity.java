a4312ed247be62936c7f03e434bf6c2ea4d4c4d9
gfigueroa <gfigueroa@almeraim.com>
2019-06-10 11:03:43 -0500

    Se actualizo libreria util por acceso a internet

--- app/src/main/java/com/almera/utilalmera/MainActivity.java
+++ app/src/main/java/com/almera/utilalmera/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spannable;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.almera.utilalmeralib.archivosutil.LibArchivosUtil;
+import com.almera.utilalmeralib.picasso.ImageDownload;
+import com.almera.utilalmeralib.picasso.LibFinishDowload;
 import com.almera.utilalmeralib.picasso.LibPicassoImageGetter;
 import com.almera.utilalmeralib.picasso.LibPicassoImageDownload;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private TextView imagenGetter;
@@ -24,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
         imagenGetter = findViewById(R.id.picassogetter);
         LibPicassoImageGetter imageGetter = new LibPicassoImageGetter(imagenGetter, MainActivity.this, idiamge);
         //String html1="<p> </p>\r\n\r\n<h1>Hola mundo<\/h1>\r\n\r\n<p><img alt=\"\" src=\"http:\/\/192.168.1.28\/sgi\/tmp\/uploads\/almera_pruebasLOCAL\/download.jpeg\" style=\"width: 284px; height: 177px;\" \/><\/p>\r\n";
-        final String html1 = "<p> </p>\r\n\r\n<h1>Picasso Campos Etiqueta<\\/h1>\r\n\r\n<p><img alt=\"\" src=\"http://192.168.1.28/sgi/tmp/uploads/almera_pruebasLOCAL/download.jpeg\" style=\"width: 284px; height: 177px;\" /></p>\r\n";
-        new Thread(new Runnable() {
+        final String html1 = "<p> </p>\r\n\r\n<h1>Picasso Campos Etiqueta<\\/h1>\r\n\r\n<p><img alt=\"\" src=\"http://192.168.1.193/sgi/tmp/uploads/almera_pruebasLOCAL/download.jpeg\" style=\"width: 284px; height: 177px;\" /></p>\r\n";
+        final ImageDownload imageDownload=new ImageDownload(html1,idiamge);
+        LibArchivosUtil.DonwloadPicturesFromHtml(getApplicationContext(), new ArrayList<ImageDownload>(Arrays.asList(imageDownload)), new LibFinishDowload() {
             @Override
-            public void run() {
-               // LibPicassoImageDownload imageGetter = new LibPicassoImageDownload(getApplicationContext(),html1,idiamge);
+            public void onFinish(Boolean status) {
+                Log.d("a", "onFinish: ");
             }
-        }
-        ).start();
+        });
         Spannable html;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             html = (Spannable) Html.fromHtml(html1, Html.FROM_HTML_MODE_LEGACY, imageGetter, null);
