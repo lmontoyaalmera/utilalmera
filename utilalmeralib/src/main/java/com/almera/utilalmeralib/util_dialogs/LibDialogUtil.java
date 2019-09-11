fcd907bb488e33162d14b356061825b15a2a94ff
gfigueroa <gfigueroa@almeraim.com>
2019-09-11 10:19:55 -0500

    Migracion dialog2

--- utilalmeralib/src/main/java/com/almera/utilalmeralib/util_dialogs/LibDialogUtil.java
+++ utilalmeralib/src/main/java/com/almera/utilalmeralib/util_dialogs/LibDialogUtil.java
@@ -1,16 +1,50 @@
 package com.almera.utilalmeralib.util_dialogs;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.almera.utilalmeralib.R;
 import com.almera.utilalmeralib.viewUtil.LibKeyboardUtil;
 import com.forms.sti.progresslitieigb.Inteface.IProgressLoadingIGB;
 import com.forms.sti.progresslitieigb.Model.JSetting;
 import com.forms.sti.progresslitieigb.ProgressLoadingJIGB;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textview.MaterialTextView;
 
 public class LibDialogUtil {
     public static LibDialogLisener showProgressDialog(final Context context, String message){
+
+
+        MaterialAlertDialogBuilder build=new MaterialAlertDialogBuilder(context);
+        ConstraintLayout constraintLayout= (ConstraintLayout) LayoutInflater.from(context).inflate(R.layout.progress_dialog,null);
+        AppCompatTextView textView=constraintLayout.findViewById(R.id.textview);
+        textView.setText(message);
+        build.setView(constraintLayout);
+        Dialog progressDialog=build.create();
+        return  new LibDialogLisener() {
+            @Override
+            public void hideDialog() {
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void showDialog() {
+                progressDialog.show();
+            }
+
+            @Override
+            public void changeMessage(String message) {
+                textView.setText(message);
+            }
+        };
+    }
+    public static LibDialogLisener showProgressDialogOver(final Context context, String message){
 
         ProgressLoadingJIGB.setupLoading = (setup) ->  {
             setup.srcLottieJson =R.raw.loading; // Tour Source JSON Lottie
